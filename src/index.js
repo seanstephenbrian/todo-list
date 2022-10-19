8a53f4dd80ad1ad27077f9f16660ab82cdfcdb1d
sean stephen brian <seanstephenbrian@gmail.com>
2022-10-19 11:45:02 -0400

    Set up project with webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const element = document.createElement('div');
+element.classList.add('test');
+document.body.appendChild(element);
+element.innerText = 'test test test test test';
